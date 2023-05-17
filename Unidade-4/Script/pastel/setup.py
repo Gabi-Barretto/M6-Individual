a8c1a5dea001262fef6aea720aa2bb2eaa09710d
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2023-05-17 11:03:07 -0300

    Atualização - Ponderada 2
    package

---
+++ Unidade-4/Script/pastel/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'pastel'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gabi-barretto',
+    maintainer_email='gabi-barretto@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'nodo_controle = pastel.controle:main',
+            'nodo_odometro = pastel.odometro:main'
+        ],
+    },
+)
